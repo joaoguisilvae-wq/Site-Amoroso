926d8f111ba57cfc283f4bc023ea73cdd11d74dd
João <joaogui.silvae@gmail.com>
2026-03-31 23:46:11 -0300

    fix: replace Flickr embed script with iframe to resolve CORS issues

--- src/routes/Fotos.tsx
+++ src/routes/Fotos.tsx
@@ -1,46 +1,33 @@
-import { useEffect } from "react";
 import "./Fotos.css";
 
 const Fotos = () => {
-  // Carregar o script do Flickr quando o componente for montado
-  useEffect(() => {
-    // Verifica se o script já foi carregado
-    if (
-      !document.querySelector(
-        'script[src="//embedr.flickr.com/assets/client-code.js"]',
-      )
-    ) {
-      const script = document.createElement("script");
-      script.src = "//embedr.flickr.com/assets/client-code.js";
-      script.async = true;
-      script.charset = "utf-8";
-      document.body.appendChild(script);
-    }
-  }, []);
-
   return (
     <div className="fotos-container">
       <h1 className="fotos-title">Nossas Fotos</h1>
 
       <div className="fotos-gallery">
         <div className="flickr-album">
-          <a
-            data-flickr-embed="true"
-            href="https://www.flickr.com/photos/204491296@N08/albums/72177720332834106"
-            title="aaaaa"
-          >
-            <img
-              src="https://live.staticflickr.com/31337/55181201240_effd6e0462_w.jpg"
-              width="400"
-              height="300"
-              alt="Nossas Fotos"
-            />
-          </a>
+          <iframe
+            src="https://www.flickr.com/photos/204491296@N08/albums/72177720332834106/player/"
+            width="800"
+            height="600"
+            frameBorder="0"
+            allowFullScreen
+            title="Nossas Fotos no Flickr"
+          />
         </div>
       </div>
 
       <div className="fotos-info">
         <p>Veja todas as nossas fotos no álbum do Flickr!</p>
+        <a
+          href="https://www.flickr.com/photos/204491296@N08/albums/72177720332834106"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flickr-link"
+        >
+          Abrir álbum completo no Flickr
+        </a>
       </div>
     </div>
   );
